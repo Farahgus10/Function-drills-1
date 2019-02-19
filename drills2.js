f7cb48ce0316354970fc11bf24fed75a767c9438
David Bolin <david@bolin.at>
2019-02-19 12:35:24 -0800

    made function return message

--- drills2.js
+++ drills2.js
@@ -1,6 +1,7 @@
 'use strict';
 function createGreeting() {
-  console.log('Hello, my name is Farah and I\'m 24 years old.');
+  return 'Hello, my name is Farah and I\'m 24 years old.';
 }
 
-createGreeting();
+const greeting1 = createGreeting();
+console.log(greeting1);
