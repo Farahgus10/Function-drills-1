e3a5d091b3428252b0bd1e178ae7de0e8d61dd56
likejackonfire <gravitybringsmedown@gmail.com>
2019-02-19 13:29:47 -0800

    checked for invalid age or name

--- drills2.js
+++ drills2.js
@@ -9,11 +9,20 @@ function getYearOfBirth(age){
 
 function createGreeting(name, age) {
   const yob = getYearOfBirth(age);
+
+  if (name === undefined || age === undefined) {
+    throw new Error('Arguments not valid');
+  }
+
+  if (typeof age !== 'number' || typeof name !== 'string') {
+    throw new TypeError();
+  }
+
   return `Hello, my name is ${name} and I'm ${age} years old. I was born in ${yob}.`;
 }
 
 try {
-const greeting1 = createGreeting('Bob', -6);
+const greeting1 = createGreeting('Henry', 6);
 console.log(greeting1);
-} catch(e) {console.error('You should not exist');
+} catch(e) {console.error(e);
 }
