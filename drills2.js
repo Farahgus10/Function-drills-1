70b41c2cbae14fa2ddae437d656f136d9f0d36e3
likejackonfire <gravitybringsmedown@gmail.com>
2019-02-19 12:51:51 -0800

    added getYearOfBirth function

--- drills2.js
+++ drills2.js
@@ -1,7 +1,12 @@
 'use strict';
+
+function getYearOfBirth(age){
+  return 2019 - age;
+}
+
 function createGreeting(name, age) {
-  const yearOfBirth = 2019 - age;
-  return `Hello, my name is ${name} and I'm ${age} years old. I was born in ${yearOfBirth}.`;
+  const yob = getYearOfBirth(age);
+  return `Hello, my name is ${name} and I'm ${age} years old. I was born in ${yob}.`;
 }
 
 const greeting1 = createGreeting();
